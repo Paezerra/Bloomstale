eae144ef8300d4a3f6d106c2dca7520a8d252aec
Camilo Paez <camilopaezescobar@gmail.com>
2024-03-17 14:40:30 -0500

    Hero Section 2 added to the component and setted up for responsive all around

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import './App.css'
 import Menu from './components/header'
 import Hero from './components/heroSections';
 import CtaSection from './components/ctaSections';
+import { HeroNames } from './components/heroSections';
 
-import bannerImage from '../public/bannerWebPage.png'
+import bannerImage from '/bannerWebPage.png'
+import daisyImage from '../public/FlowerGrdi.png'
 
 function App() {
   const [languageStatus, setLanguageStatus] = useState('ES')
@@ -16,8 +18,9 @@ function App() {
   return (
     <>
       <Menu languageStatus={languageStatus} selectLanguage={selectLanguage}></Menu>
-      <Hero type='hero_section_1-1' title='Bloomstale, arte floral' primaryText='Contamos historias con Flores' secondaryText='Somos un estudio de arte centrado en la creación de cuadros Oshibana o Arte de flores prensadas ' cta1='Conoce nuestra Historia' cta2='Ir al Catálogo' thirdText='Arte Oshibana - Cuadros Florales - Preservación de flores' image={bannerImage}></Hero>
+      <Hero type={HeroNames.HeroMain_1} title='Bloomstale, arte floral' primaryText='Contamos historias con Flores' secondaryText='Somos un estudio de arte centrado en la creación de cuadros Oshibana o Arte de flores prensadas ' cta1='Conoce nuestra Historia' cta2='Ir al Catálogo' thirdText='Arte Oshibana - Cuadros Florales - Preservación de flores' image={bannerImage} imageAlt='Imagen de una flor disecada con petalos morados y blancos'></Hero>
       <CtaSection type='triple_button' primaryText='OBTEN 10% OFF EN 1° PEDIDO' secondaryText='ORIGEN RESPONSABLE' thirdText='ENVIOS Y EMPAQUES'></CtaSection>
+      <Hero type={HeroNames.HeroImage_1} image={daisyImage} primaryText='Más que unas flores bonitas' secondaryText='Regala flores y crea alegría'></Hero>
     </>
   )
 }
--- src/components/heroSections/index.tsx
+++ src/components/heroSections/index.tsx
@@ -1,6 +1,11 @@
 import styles from './index.module.css'
 import ButtonsComponent from '../buttons/button';
-import {ButtonNames} from '../buttons/button';
+import { ButtonNames } from '../buttons/button';
+
+export enum HeroNames {
+    HeroMain_1 = 'HeroSectionMain_1',
+    HeroImage_1 = 'HeroSectionImage_1',
+}
 
 type Props = {
     type?: string;
@@ -9,18 +14,21 @@ type Props = {
     secondaryText?: string;
     thirdText?: string;
     image?: string;
+    imageAlt?: string;
     cta1?: string;
     cta2?: string;
+    cta1Link?: string;
+    cta2Link?: string;
 };
 
-const Hero: React.FC<Props> = ({ type, title, primaryText, secondaryText, thirdText, image, cta1, cta2 }) => {
+const Hero: React.FC<Props> = ({ type, title, primaryText, secondaryText, thirdText, image, imageAlt, cta1, cta2, cta1Link, cta2Link }) => {
 
     let hero_section
 
-    if (type === 'hero_section_1-1') {
+    if (type === HeroNames.HeroMain_1) {
         hero_section =
             <section className={styles.hero}>
-                <title>{title}</title>
+                {title && <title>{title}</title>}
                 <div className={styles.dividerLeft}>
                     <h3 className={styles.invertedText}>{thirdText}</h3>
                 </div>
@@ -29,17 +37,29 @@ const Hero: React.FC<Props> = ({ type, title, primaryText, secondaryText, thirdT
                         <h1>{primaryText}</h1>
                         <h2>{secondaryText}</h2>
                         <div className={styles.buttonContainer}>
-                            <ButtonsComponent label={cta1} type={ButtonNames.GreenFilledButton} link=''></ButtonsComponent>
-                            <ButtonsComponent label={cta2} type={ButtonNames.GreenEmptyButton}></ButtonsComponent>
+                            <ButtonsComponent label={cta1} type={ButtonNames.GreenFilledButton} link={cta1Link}></ButtonsComponent>
+                            <ButtonsComponent label={cta2} type={ButtonNames.GreenEmptyButton} link={cta2Link}></ButtonsComponent>
                         </div>
                     </ul>
-                    <img src={image} alt="Imagen de una flor disecada con petalos morados y blancos" />
+                    <img src={image} alt={imageAlt} />
+                </div>
+                <div className={styles.dividerleftrow}></div>
+            </section>
+    } else if (type === HeroNames.HeroImage_1) {
+        hero_section =
+            <section className={styles.hero}>
+                {title && <title>{title}</title>}
+                <div></div>
+                <div className={styles.heroContentImage}>
+                    <img src={image} alt={imageAlt} />
+                    <div className={styles.textContainerImage}>
+                        <h3>{primaryText}</h3>
+                        <h3>{secondaryText}</h3>
+                    </div>
                 </div>
                 <div className={styles.dividerleftrow}></div>
             </section>
     }
-
-
     return (
         <>
             {hero_section}
